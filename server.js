d96b339b5c34a88a4adf958f55da31c258764a4b
Aviwe Ngqukumba <aviwe3226@gmail.com>
2018-07-19 16:34:03 +0200

    change host to connectionString

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const imageEntry = require('./controllers/imageEntry');
 const db = knex({
     client: 'pg',
     connection: {
-      host : process.env.DATABASE_URL,
+      connectionString : process.env.DATABASE_URL,
       ssl: true
     }
   });
